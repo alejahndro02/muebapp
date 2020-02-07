6958400e1abb9a6595d12a1ebf0d6f1bc67ba866
alejahndro02 <alejahndro02@gmail.com>
2020-02-07 13:46:14 -0600

    Se agrego el route para el componente AddUsers.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 // import { Grid } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid'
-import theme from './theme/theme'
 import './App.css';
 // import { render } from '@testing-library/react';
 import Furniture from './components/vistas/Furniture'
 import AppNavbar from './components/layout/AppNavbar'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
+import theme from './theme/theme'
+import AddUsers from './components/security/AddUsers'
+
 class App extends Component {
     render() {
         return ( 
@@ -16,8 +18,8 @@ class App extends Component {
                 <AppNavbar / >
                 <Grid container >
                     <Switch >
-                    <Route path = "/"
-                    exact component = { Furniture }/>   
+                        <Route path = "/" exact component = { Furniture }/>
+                        <Route path = "/auth/adduser" exact component = { AddUsers }/>
                     </Switch > 
                 </Grid > 
             </MuiThemeProvider > 
