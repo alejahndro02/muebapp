a8d4b51026e5877849dfb0b530114d72e9777bd6
alejahndro02 <alejahndro02@gmail.com>
2020-02-06 21:08:05 -0600

    Elaboracion del archivo AppNavbar

---
+++ src/components/layout/AppNavbar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import AppBar from '@material-ui/core/AppBar'
+
+class AppNavbar extends Component {
+    render() {
+        return ( <
+            div >
+            <
+            AppBar position = "static" >
+
+            <
+            /AppBar> < /
+            div >
+        );
+    }
+}
+
+export default AppNavbar;
