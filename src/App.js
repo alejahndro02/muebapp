7de31c78c8c2b1e1123b8d48f135a7a9006d6179
alejahndro02 <alejahndro02@gmail.com>
2020-02-06 21:04:20 -0600

    instalacion de la libreria react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom'
 import './App.css';
 // import { render } from '@testing-library/react';
 import Furniture from './components/vistas/Furniture'
-
+import AppNavbar from './components/layout/AppNavbar'
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
+import theme from './theme/theme'
 class App extends Component {
     render() {
         return ( <
+            AppNavbar >
+            <
+            MuiThemeProvider theme = { theme } >
+            <
             Furniture / >
+            <
+            /MuiThemeProvider> < /
+            AppNavbar >
         )
     }
 }
